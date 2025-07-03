0ec96ff595f9a052ce5e64cd8607dd04b558fb10
jthai <jthai@gmail.com>
2025-07-03 12:27:52 -0700

    CircleShape

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,7 +20,9 @@ def main():
 
         screen.fill((0, 0, 0))
 
-        pygame.display.flip()        
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000        
 
 if __name__ == "__main__":
     main()
